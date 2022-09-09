d42b49cc6a37194ae1fa4232b15521b8544fddf3
emirahmtv <emir.ahmtv@eduge.ch>
2022-09-09 11:32:51 +0200

    09.09.2022 first update

--- views/header.php
+++ views/header.php
@@ -1,43 +1,43 @@
 <link rel="stylesheet" href="css/header.css">
 <div class="navigation">
-  <div class="logo">
-    <a class="no-underline" href="#">
-      Instakg
-    </a>
-  </div>
-  <div class="navigation-search-container">
-    <i class="fa fa-search"></i>
-    <input class="search-field" type="text" placeholder="Search">
-    <div class="search-container">
-      <div class="search-container-box">
-        <div class="search-results">
-
-        </div>
-      </div>
+    <div class="logo">
+        <a class="no-underline" href="#">
+            Instakg
+        </a>
     </div>
-  </div>
-  <div class="navigation-icons">
-  <div class="logo">
-    <a class="no-underline" href="#">
-      Messages
-    </a>
-  </div>
-    <a class="navigation-link notifica">
-      <i class="far fa-heart">
-        <div class="notification-bubble-wrapper">
-          <div class="notification-bubble">
-            <span class="notifications-count">99</span>
-          </div>
+    <div class="navigation-search-container">
+        <i class="fa fa-search"></i>
+        <input class="search-field" type="text" placeholder="Search">
+        <div class="search-container">
+            <div class="search-container-box">
+                <div class="search-results">
+
+                </div>
+            </div>
         </div>
-      </i>
-    </a>
-    <a href="#" class="navigation-link">
-      <i class="far fa-user-circle"></i>
-    </a>
-    <a href="#" id="signout" class="navigation-link">
-      <i class="fas fa-sign-out-alt"></i>
-    </a>
-  </div>
+    </div>
+    <div class="navigation-icons">
+        <div class="logo">
+            <a class="no-underline" href="#">
+                Messages
+            </a>
+        </div>
+        <a class="navigation-link notifica">
+            <i class="far fa-heart">
+                <div class="notification-bubble-wrapper">
+                    <div class="notification-bubble">
+                        <span class="notifications-count">99</span>
+                    </div>
+                </div>
+            </i>
+        </a>
+        <a href="#" class="navigation-link">
+            <i class="far fa-user-circle"></i>
+        </a>
+        <a href="#" id="signout" class="navigation-link">
+            <i class="fas fa-sign-out-alt"></i>
+        </a>
+    </div>
 </div>
 
 <!-- scroll down test--->
