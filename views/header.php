19d93edba8abaa694174b050eeac137326b440a5
leotrnn <leo.trn@eduge.ch>
2022-09-09 11:26:40 +0200

    fini header

--- views/header.php
+++ views/header.php
@@ -5,10 +5,10 @@
       Instakg
     </a>
   </div>
-  <div class="navigation-search-container">
-    <i class="fa fa-search"></i>
-    <input class="search-field" type="text" placeholder="Search">
+  <div class="navigation-search-container">   
+    <input class="search-field" type="text" placeholder="Rechercher">
     <div class="search-container">
+    
       <div class="search-container-box">
         <div class="search-results">
 
@@ -16,30 +16,18 @@
       </div>
     </div>
   </div>
-  <div class="navigation-icons">
+  <div class="navigation-icons"> 
+    <div class="logo">
+    <img src="img/home.png" width="35">
+  </div>
   <div class="logo">
-    <a class="no-underline" href="#">
-      Messages
-    </a>
+    <img src="img/msg.png" width="35">
   </div>
-    <a class="navigation-link notifica">
-      <i class="far fa-heart">
-        <div class="notification-bubble-wrapper">
-          <div class="notification-bubble">
-            <span class="notifications-count">99</span>
-          </div>
-        </div>
-      </i>
-    </a>
-    <a href="#" class="navigation-link">
-      <i class="far fa-user-circle"></i>
-    </a>
-    <a href="#" id="signout" class="navigation-link">
-      <i class="fas fa-sign-out-alt"></i>
-    </a>
+  <div class="logo">
+    <img src="img/upload.png" width="35">
   </div>
-</div>
-
-<!-- scroll down test--->
-<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-<!-- scroll down test--->
+  <div class="logo">
+    <img src="img/round.png" width="35">
+  </div>
+ 
+   
