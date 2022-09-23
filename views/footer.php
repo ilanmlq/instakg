2c96508d7c848de4871c9e7ea3800af53572308c
leotrnn <leo.trn@eduge.ch>
2022-09-23 09:13:05 +0200

    footer

--- views/footer.php
+++ views/footer.php
@@ -1,11 +1,6 @@
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
-<link rel="stylesheet" href="views/css/footer.css">
-
-
-<body>
     <footer class="bg-light text-center text-lg-start">
         <!-- Copyright -->
-        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+        <div class="text-center p-3">
             ©
             <a class="text-dark" href="https://mdbootstrap.com/">Instakg</a>
         </div>
