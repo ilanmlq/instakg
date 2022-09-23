a23ad4befdf6d3b67bc477465f03a4ac2b9d6d46
emirahmtv <emir.ahmtv@eduge.ch>
2022-09-23 09:25:23 +0200

    login et register

--- views/auth/register.php
+++ views/auth/register.php
@@ -1,39 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
+<form action="index.php?url=acce">
+    <div class="container-auth">
+        <h1>S'inscrire</h1>
+        <p>Veuillez remplir toutes les données pour pouvoir s'inscrire.</p>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="">
-</head>
+        <hr>
 
-<body>
-    <form action="/action_page.php">
-        <div class="container">
-            <h1>Register</h1>
-            <p>Please fill in this form to create an account.</p>
-            <hr>
+        <label for="email"><b>Email</b></label>
+        <input type="email" placeholder="Votre Email" name="email" value="">
 
-            <label for="email"><b>Email</b></label>
-            <input type="text" placeholder="Enter Email" name="email" id="email" required>
+        <label for="username"><b>Nom d'utilisateur</b></label>
+        <input type="text" placeholder="Votre nom" name="username" value="">
 
-            <label for="psw"><b>Password</b></label>
-            <input type="password" placeholder="Enter Password" name="psw" id="psw" required>
+        <label for="pwd"><b>Mot de passe</b></label>
+        <input type="password" placeholder="Votre mot de passe" name="pwd">
 
-            <label for="psw-repeat"><b>Repeat Password</b></label>
-            <input type="password" placeholder="Repeat Password" name="psw-repeat" id="psw-repeat" required>
-            <hr>
-            <p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>
+        <label for="pwdRepeat"><b>Votre mot de passe à nouveau</b></label>
+        <input type="password" placeholder="Confirmer votre mot de passe" name="pwdRepeat">
+        
+        <hr>
 
-            <button type="submit" class="registerbtn">Register</button>
-        </div>
+        <p>En créant un compte vous acceptez <a href="#">nos conditions</a>.</p>
 
-        <div class="container signin">
-            <p>Already have an account? <a href="#">Sign in</a>.</p>
-        </div>
-    </form>
-</body>
+        <button type="submit" class="registerbtn">S'inscrire</button>
+    </div>
 
-</html>
+    <div class="container signin">
+        <p>Déjà un compte ? <a href="#">Se connecter</a>.</p>
+    </div>
+</form>
