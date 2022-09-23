ea466b57610a734865931bc9913eff89979e59b6
leotrnn <leo.trn@eduge.ch>
2022-09-23 09:18:02 +0200

    index

--- index.php
+++ index.php
@@ -2,19 +2,27 @@
 
 session_start();
 
-$url = filter_input(INPUT_GET,'url', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$url = filter_input(INPUT_GET, 'url', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
 require_once 'views/header.php';
 
-if (empty($url)) {  
+if (empty($url)) {
     $url = 'home';
 }
 
-switch($url){
-    case 'home' :
+switch ($url) {
+    case 'home':
         include('views/home.php');
         break;
-
+    case 'msg':
+        include('views/msg.php');
+        break;
+    case 'upload':
+        include('views/upload.php');
+        break;
+    case 'profile':
+        include('views/profile.php');
+        break;
 }
 
 require_once 'views/footer.php';
