b7a57d3d20a3f708dd40d88e2c5e2ccf6bae70ea
leotrnn <leo.trn@eduge.ch>
2022-09-23 09:38:11 +0200

    footer

--- views/footer.php
+++ views/footer.php
@@ -1,10 +1,7 @@
     <footer class="bg-light text-center text-lg-start">
-        <!-- Copyright -->
         <div class="text-center p-3">
-            ©
-            <a class="text-dark" href="https://mdbootstrap.com/">Instakg</a>
+            <a class="text-dark">©  Instakg</a>
         </div>
-        <!-- Copyright -->
     </footer>
 </body>
 
